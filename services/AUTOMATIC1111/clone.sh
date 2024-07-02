bbdfebea707908580a0569c3f8100e623d9a8880
Gil Assuncao <gil.assuncao@gmail.com>
2024-07-02 22:07:52 +0100

    Update to v1.9.4 and some cleanup

--- services/AUTOMATIC1111/clone.sh
+++ services/AUTOMATIC1111/clone.sh
@@ -2,10 +2,19 @@
 
 set -Eeuox pipefail
 
+echo "Creating folder..."
 mkdir -p /repositories/"$1"
 cd /repositories/"$1"
+
+echo "Cloning repository..."
 git init
 git remote add origin "$2"
+
+echo "Updating repository..."
 git fetch origin "$3" --depth=1
+
+echo "Set repository to specific version..."
 git reset --hard "$3"
+
+echo "Removing .git folder..."
 rm -rf .git
